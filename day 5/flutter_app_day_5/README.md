0d54931aaad4487468b1d2c89642e2dd127d3cb0
Mohammed <Mohammed.hafiz.2710@gmail.com>
2024-08-20 21:34:10 +0300

    Day 5

---
+++ day 5/flutter_app_day_5/README.md
@@ -0,0 +1,3 @@
+# flutter_app_day_5
+
+A new Flutter project.
---
+++ day 5/flutter_app_day_5/lib/screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatelessWidget {
+  const Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 60, 31, 66),
+        shadowColor: Colors.black,
+        elevation: 8,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+        title: const Center(
+          child: Text(
+            "Log in",
+            style: TextStyle(color: Colors.white, fontSize: 26),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          const Center(
+            child: Image(
+              image: AssetImage(
+                "assets/computer-icons-user-profile-password-login-png-favpng-a5KCGVCAsuair5v9BArYfpjLK-removebg.png",
+              ),
+              width: 150,
+              height: 150,
+            ),
+          ),
+          const SizedBox(height: 28),
+          SizedBox(
+            width: 300, // Adjust the width as needed
+            child: TextFormField(
+              decoration: InputDecoration(
+                label: const Text("User Name"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              style:
+                  const TextStyle(fontSize: 24), // Adjust text size if needed
+            ),
+          ),
+          const SizedBox(height: 16), // Add space between the fields
+          SizedBox(
+            width: 300,
+            child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  label: const Text("Password"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                style: const TextStyle(fontSize: 24)),
+          ),
+        ],
+      ),
+    );
+  }
+}
